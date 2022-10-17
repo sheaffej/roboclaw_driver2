99f9341ceb74cc3660392a8429ad721ad5abea44
sheaffej <sheaffej@gmail.com>
2022-10-16 20:09:34 -0700

    add launch to setup.py data_files

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'roboclaw_driver2'
@@ -7,9 +9,10 @@ setup(
     version='2.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
